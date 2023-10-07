7a6dc84761d13baa1637fc8cb9492a92ae75adc3
sandaruwan12345 <146765452+sandaruwan12345@users.noreply.github.com>
2023-10-07 10:21:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,20 +14,20 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['94764497078'] //ur owner number
-global.ownername = "Sandaru Nethsara" //ur owner name
-global.ytname = "YT: Sandaru Yt" //ur yt chanel name
+global.owner = ['94729569031'] //ur owner number
+global.ownername = "Nr Rashu Modz" //ur owner name
+global.ytname = "YT: NR RASHU MODZ " //ur yt chanel name
 global.socialm = "GitHub: Mr-Katussa" //ur github or insta name
 global.location = "Sri Lanka, Colombo, Kiribathgoda" //ur location
 
 //bot bomdy 
-global.ownernomer = "94764497078" //ur number
-global.premium = ['94764497078'] //ur premium number
-global.botname = 'Xcoco' //ur bot name
+global.ownernomer = "94779122081" //ur number
+global.premium = ['94779122081'] //ur premium number
+global.botname = 'NR RASHU MODZ' //ur bot name
 global.themeemoji = "♻️" //ur theme emoji
-global.packname = "Sticker By Xcoco" //ur sticker watermark packname
-global.author = "Xcoco" //ur sticker watermark author
-global.wm = "XCOCO BOT INC" //ur watermark
+global.packname = "Sticker By RASHU" //ur sticker watermark packname
+global.author = "RASHU" //ur sticker watermark author
+global.wm = "RASHU BOT NR" //ur watermark
 
 // Other
 global.sessionName = 'session'
